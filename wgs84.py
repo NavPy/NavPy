9fa927854bf015a7ea8d7faf47068ced7f7c5101
Adhika Lie <adhika.lee@gmail.com>
2013-11-06 14:13:33 -0600

    Added wgs84.py as a module containing all the wgs84 constants
    Added earthrad function to calculate the earth's radius.
    Added llarate function to calculate the latitude, longitude, and altitude
    rate given Earth relative velocity and position.

---
+++ wgs84.py
@@ -0,0 +1,14 @@
+"""
+WGS84 constants
+Reference:
+Paul Groves
+http://www.oosa.unvienna.org/pdf/icg/2012/template/WGS_84.pdf
+"""
+
+R0 = 6378137.0
+f = 298.257223563
+omega_E = 7.292115e-5
+GM = 3.986004418e14
+
+ecc = 0.0818191908425
+Rp = 6356752.314245
