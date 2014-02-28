4391e2da940f886eaf83c9f220a3d3edc76aa895
Adhika Lie <adhika.lee@gmail.com>
2014-02-28 14:46:27 -0600

    Added copyright notice on navpy.py and wgs84.py

--- navpy.py
+++ navpy.py
@@ -1,3 +1,9 @@
+"""
+Copyright (c) 2014 NavPy Developers. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be found in
+LICENSE.txt
+"""
+
 import numpy as np
 import wgs84
 
