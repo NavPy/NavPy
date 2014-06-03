b663913549faca96e5a7fed8f6c7898ac4c9cb2c
Adhika Lie <adhika.lee@gmail.com>
2014-06-03 15:00:34 -0500

    Added earth rotation rate for GPS according to ICD-200.

--- wgs84.py
+++ wgs84.py
@@ -52,6 +52,9 @@ Examples
 a = 6378137.0 # Semi-major Axis [m]
 f = 1./298.257223563 # Flattening
 omega_E = 7292115.0e-11 # Angular velocity of the Earth [rad/s]
+omega_E_GPS = 7292115.1467e-11 # Angular velocity of the Earth [rad/s]
+                              # According to ICD-GPS-200
+
 GM = 3986004.418e8 # Earth's Gravitational Constant [m^3/s^2]
                    # (mass of earth's atmosphere included)
 
