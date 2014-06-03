9993085b0101ed3055341644a3ff611901561a4e
Hamid <mokh0006@umn.edu>
2014-06-03 15:57:58 -0500

    Remove dependency on old file (nav.py).

--- navpyTest.py
+++ navpyTest.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
-import nav # TODO (Hamid) this eventually be removed... we are migrating to navpy
+"""
+Copyright (c) 2014 NavPy Developers. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be found in
+LICENSE.txt
+"""
 import navpy
 import unittest
 import numpy as np
